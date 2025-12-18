7b76c15752eda7f71c160231d620fffbfb987cf8
Stack Quantum <stackquantum1@gmail.com>
2025-12-18 06:59:27 -0800

    Updating User Interface

--- src/script.js
+++ src/script.js
@@ -1,6 +1,13 @@
   const sidebar = document.getElementById('sidebar');
+  const overlay = document.getElementById('sidebar-overlay');
   const toggleBtn = document.getElementById('sidebar-toggle');
 
   toggleBtn.addEventListener('click', () => {
     sidebar.classList.toggle('-translate-x-full');
+    overlay.classList.toggle('hidden');
+  });
+
+  overlay.addEventListener('click', () => {
+    sidebar.classList.add('-translate-x-full');
+    overlay.classList.add('hidden');
   });
